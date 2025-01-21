8cb201eea7e15c687e39e16b85786324b6d9ad8b
Sasksham Chouhan <surinder9876kumar@gmail.com>
2025-01-22 01:10:47 +0300

    Updates

---
+++ precache-manifest.f8241a29772c4f219724c1e35ea9a6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277342d84dc98a05443526e30e44bf90",
+    "url": "/ai-pdf-extract/index.html"
+  },
+  {
+    "revision": "7cb358cea86b65ca997d",
+    "url": "/ai-pdf-extract/static/css/main.120a573a.chunk.css"
+  },
+  {
+    "revision": "7599cdbb2d5dc7098156",
+    "url": "/ai-pdf-extract/static/js/2.6cf622a5.chunk.js"
+  },
+  {
+    "revision": "73e88762c69a3c614405",
+    "url": "/ai-pdf-extract/static/js/3.c02789ba.chunk.js"
+  },
+  {
+    "revision": "7cb358cea86b65ca997d",
+    "url": "/ai-pdf-extract/static/js/main.ca3a1eda.chunk.js"
+  },
+  {
+    "revision": "6f40d4e2be3d6deacb5e",
+    "url": "/ai-pdf-extract/static/js/runtime~main.1beb287f.js"
+  }
+]);
